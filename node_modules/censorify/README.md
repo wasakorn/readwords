c004fb401571320ec85243c2351a3107df6ccb1d
Wasakorn Pakdeesan <wasakorn.pakdeesan@scb.co.th>
2018-09-06 11:58:56 +0700

    First version

---
+++ node_modules/censorify/README.md
@@ -0,0 +1,1 @@
+Censor word from defined words
---
+++ readwords.js
@@ -0,0 +1,5 @@
+var censor = require("censorify");
+console.log(censor.getCensoredWords());
+console.log(censor.censor("Some very sad, bad and mad text."));
+censor.addCensoredWord("gloomy");
+console.log(censor.censor("A very gloomy day."));
